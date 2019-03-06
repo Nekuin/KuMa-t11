5e466ed29fb6295643877d69262ec2c75b85805f
Nekuin <Nekuin@gmail.com>
2019-03-06 18:54:31 +0200

    Update CommentMain.java

--- src/app/CommentMain.java
+++ src/app/CommentMain.java
@@ -3,18 +3,6 @@ package app;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CommentMain {
 
